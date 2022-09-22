a576d72c5fdf99305eefab1e51df22134d758085
Yohanna Oliveira <yohannaoliveira50@gmail.com>
2022-09-22 20:53:50 -0300

    Category memory

---
+++ src/test/java/com/devsuperior/projectproduct/ProjectproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.projectproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
